0618ddd8022ffd9bc71c55f496bbfbe03f109cef
future4ing <mirae4120@gmail.com>
2024-06-28 15:10:33 +0900

    feat : 채팅 기능 준비 - 같은 메시지를 돌려주는 기능 추가

--- src/server.js
+++ src/server.js
@@ -20,9 +20,9 @@ wss.on("connection", (socket) => {
     console.log("Connected to Browser");
     socket.on("close", () => console.log("Disconnected from Browser"));
     socket.on("message", (message) => {
-        console.log(`${message}`)
+        socket.send(`${message}`)
     });
-    socket.send("Hello!!");
+    
 });
 
 server.listen(3000, handleListen);
