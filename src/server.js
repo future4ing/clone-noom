f5145e95eee7c020bd2bfc6686aabd916a47ed82
future4ing <mirae4120@gmail.com>
2024-06-28 13:07:01 +0900

    front : 뷰를 위한 스크립트 추가

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
+app.arguments("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
