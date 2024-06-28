3b4b07cf9f949cb4fda6a4cba119396f1a21b3fd
future4ing <mirae4120@gmail.com>
2024-06-28 15:14:51 +0900

    feat: 사용자간 채팅하기 - 가짜 데이터베이스 만들기

--- src/server.js
+++ src/server.js
@@ -16,7 +16,10 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const socket = [];
+
 wss.on("connection", (socket) => {
+    WebSocketServer.push(socket);
     console.log("Connected to Browser");
     socket.on("close", () => console.log("Disconnected from Browser"));
     socket.on("message", (message) => {
