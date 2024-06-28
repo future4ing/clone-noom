cffe343f186aacca42b66ad702912394f3db3c1a
future4ing <mirae4120@gmail.com>
2024-06-28 16:06:58 +0900

    feat: 닉네임추가2 - 메시지에 닉네임 표시하기

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ wss.on("connection", (socket) => {
         const message = JSON.parse(msg);
         switch(message.type){
             case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${message.payload}`));
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`));
                 break;
             case "nickname":
                 socket["nickname"] = message.payload;
